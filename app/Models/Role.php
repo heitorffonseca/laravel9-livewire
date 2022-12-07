b7204c1987e7837b2c90f9c7e7663d41a44ce957
Heitor Fonseca <heitorffonseca@gmail.com>
2022-12-06 23:12:20 -0300

    config models

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,43 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Carbon;
 
+/**
+ * Class Role
+ * @package App\Models
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $uuid
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ *
+ * @property User[] $users
+ */
 class Role extends Model
 {
     use HasFactory;
+
+    protected $table = 'roles';
+
+    protected $fillable = [
+        'uuid',
+        'name',
+        'slug'
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime'
+    ];
+
+    /**
+     * @return HasMany
+     */
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class, 'role_id', 'id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,32 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * Class User
+ * @package App\Models
+ *
+ * @property integer $id
+ * @property string $uuid
+ * @property integer $role_id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property boolean|integer $is_active
+ * @property Carbon $email_verified_at
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property string $profile_photo_url
+ *
+ * @property Role $role
+ */
 class User extends Authenticatable
 {
     use HasApiTokens;
@@ -18,22 +38,17 @@ class User extends Authenticatable
     use Notifiable;
     use TwoFactorAuthenticatable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var string[]
-     */
+    protected $table = 'users';
+
     protected $fillable = [
+        'uuid',
+        'role_id',
         'name',
         'email',
         'password',
+        'is_active'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password',
         'remember_token',
@@ -41,21 +56,21 @@ class User extends Authenticatable
         'two_factor_secret',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime'
     ];
 
-    /**
-     * The accessors to append to the model's array form.
-     *
-     * @var array
-     */
     protected $appends = [
         'profile_photo_url',
     ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class, 'role_id', 'id');
+    }
 }
